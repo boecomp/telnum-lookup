1726329fcbb3bbd208500e9a4cbaffbcad620d10
Björn Frischknecht <bjoern@frischknecht-online.ch>
2022-11-20 18:49:20 +0100

    first checkin

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+	/**
+	 * 
+	 * Sets up vars.
+	 * 
+	 * set the tel-search.ch Key - you can get one here: https://tel.search.ch/api/getkey
+	 * 
+	 * */
+
+	
+	$key = '2063c8bca19e2bc3eb40eff02e72cf1e';
+ ?>
---
+++ telnum.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The telnum.php is used to lookup phone numbers via the tel-search.ch API
+ *
+ * 
+ */
+
+   /** Sets up telnum vars and included files. */
+   require_once('settings.php');
+
+   //$key = '2063c8bca19e2bc3eb40eff02e72cf1e';
+
+   $lookup = ltrim('+'.$_GET['suche']);                                          
+   $uri = 'https://tel.search.ch/api/?was='.$lookup.'&key='.$key;
+   $data = file_get_contents($uri);            
+   $xml = new SimpleXmlElement($data);          
+   $i = 0;                                                                                                                                 
+   foreach ($xml->entry as $entry)
+      {
+      $namespaces = $entry->getNameSpaces(true);
+      $telnum = $entry->children($namespaces['tel']);
+         
+         /** use direct tel:var   */                                      
+         $name = $telnum->name;                             
+         $firstname = $telnum->firstname;                   
+         $street = $telnum->street;                         
+         $streetno = $telnum->streetno;                     
+         $zip = $telnum->zip ;                              
+         $city = $telnum->city ;                            
+         $phone = $telnum->phone;                           
+         if($i++ == 0)
+         /** Sets up telnup output format. */
+
+         {                                 
+            echo $name.' '.$firstname;                       
+         }
+      }                                             
+ ?>
